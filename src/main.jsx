cc6eaf73a052d4b53e1d12b5544e1d64132bc85e
GayuhJati <gayuhjati02@gmail.com>
2024-07-12 18:19:03 +0700

    update something

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename={import.meta.env.BASE_URL}>
     <App />
   </BrowserRouter>,
 )
